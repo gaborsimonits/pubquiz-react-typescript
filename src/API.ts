cb19016cd7d43341b6d5f9968d48ce11cff00b7f
gaborsimonits <gaborsimonits@gmail.com>
2022-01-24 16:32:22 +0100

    props types setup, states declared & got fetch in data

--- src/API.ts
+++ src/API.ts
@@ -1,1 +1,13 @@
 import React from "react";
+
+export enum Difficulty {
+    EASY = 'easy',
+    MEDIUM = 'medium',
+    HARD = ' hard'
+}
+
+export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+    const data = await (await fetch(endpoint)).json();
+    console.log(data)
+}
